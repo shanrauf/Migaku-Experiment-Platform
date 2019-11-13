c579912942772a28c48352d910fbeb5a7d9dc6fd
Shan Rauf <s.rauf124@gmail.com>
2019-11-12 21:45:20 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MIA Experiment API
---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+
+// CORS Middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+//Set up server
+let server = app.listen(process.env.PORT || 8080, function() {
+  let port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+// Initialize database
+// ...
+
+//GET API
+app.get("/api/users", function(req, res) {
+  // let query = "select * from users";
+  res.send("Hiya!");
+});
+
+// create user
+app.post("/api/users", function(req, res) {
+  // let query = "select * from users";
+  res.send("Success!");
+});
+
+app.get("/api/users/:user", function(req, res) {
+  let user = req.params.id;
+  // let query = "select * from [user]";
+  res.send(user);
+});
