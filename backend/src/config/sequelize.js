6167ad4b236fb2c6a59501d7240b4808412ac8d0
Shan Rauf <s.rauf124@gmail.com>
2019-12-12 08:06:29 -0800

    Add variables for prod db

--- backend/src/config/sequelize.js
+++ backend/src/config/sequelize.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "root",
+    username: process.env.ROOT_USER,
     password: process.env.ROOT_PASS,
     database: "mia_experiment",
     host: "127.0.0.1",
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.ROOT_USER_PROD,
+    password: process.env.ROOT_PASS_PROD,
+    database: process.env.DATABASE_NAME_PROD,
+    host: process.env.DATABASE_URL_PROD,
+    dialect: process.env.DATABASE_DIALECT
   }
 };
