0578eb4600c92c3dba299daf95b589ff1fb8991f
Shan Rauf <s.rauf124@gmail.com>
2019-12-14 06:17:30 -0800

    Add api proxy for netlify

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const baseDomain =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "54.197.11.54/backend"; // hard-coded for now since .env isn't in Github for Netlify to catch
+  process.env.NODE_ENV == "development"
+    ? "localhost:3000"
+    : window.location.origin;
 const baseURL = `${baseDomain}/api`;
 
 console.log(baseURL);
