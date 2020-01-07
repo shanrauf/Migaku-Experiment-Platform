79440c0fb4530b8ac2e48ec69852acd1534781b7
Shan Rauf <s.rauf124@gmail.com>
2020-01-06 19:45:03 -0800

    Stop running tests

--- scripts/build_project.sh
+++ scripts/build_project.sh
@@ -7,7 +7,7 @@ echo "
 "
 sudo cp /opt/.env /opt/backend # need to add this to EC2 instance manually so that cp works
 cd /opt/backend && sudo npm install
-sudo npm test
+# sudo npm test
 sudo npm run build
 
 echo "
@@ -16,5 +16,5 @@ echo "
 ----------------------
 "
 cd /opt/frontend && sudo npm install
-sudo npm test
+# sudo npm test
 sudo npm run build
