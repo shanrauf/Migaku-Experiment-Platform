c7b63d6a5d356b45abbffb075dd607a9dd094bc2
Shan Rauf <s.rauf124@gmail.com>
2020-01-15 01:16:48 -0800

    Update footer social links

--- frontend/src/components/BaseFooter.vue
+++ frontend/src/components/BaseFooter.vue
@@ -3,27 +3,59 @@
     <v-card class="flex" flat tile>
       <v-card-title>
         <strong class="subheading"
-          >Copyright &copy; 2019 Mass Immersion Approach</strong
+          >Copyright &copy; 2020 Mass Immersion Approach</strong
         >
 
         <v-spacer />
 
         <v-btn
+          v-for="social in socialLinks"
+          :key="social.link"
+          class="social-icon"
           icon
-          color="black"
-          href="https://github.com/shanrauf"
+          :color="social.color"
+          :href="social.link"
           target="_blank"
         >
-          <v-icon>$vuetify.icons.github</v-icon>
+          <v-icon>{{ social.icon }}</v-icon>
         </v-btn>
       </v-card-title>
     </v-card>
   </v-footer>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      socialLinks: [
+        {
+          icon: '$vuetify.icons.twitter',
+          link: 'https://twitter.com/massimmersion',
+          color: '#00acee'
+        },
+        {
+          icon: '$vuetify.icons.youtube',
+          link: 'https://www.youtube.com/user/MATTvsJapan',
+          color: '#c4302b'
+        },
+        {
+          icon: '$vuetify.icons.facebook',
+          link: 'https://www.facebook.com/MassImmersionApproach/',
+          color: '#3b5998'
+        }
+      ]
+    };
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 .base-footer {
   margin-top: 15px;
   border-top: 1px solid gray;
+}
+.social-icon {
+  margin: 0 5px 0 5px;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,32 +1,31 @@
-import Vue from "vue";
-import App from "@/App.vue";
-import router from "@/router";
-import store from "@/store";
-import "@/components/_globals";
+import Vue from 'vue';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import '@/components/_globals';
 
 Vue.config.productionTip = false;
 Vue.config.performance = true;
 
-import Notifications from "vue-notification";
+import Notifications from 'vue-notification';
 Vue.use(Notifications);
 
-import Vuetify from "vuetify/lib/framework";
-import "vuetify/dist/vuetify.min.css";
-import "material-icons";
-import "@fortawesome/fontawesome-free/css/all.css";
+import Vuetify from 'vuetify/lib/framework';
+import 'vuetify/dist/vuetify.min.css';
+import 'material-icons';
+import '@fortawesome/fontawesome-free/css/all.css';
 // import "@mdi/font/css/materialdesignicons.css";
 
 const opts = {
   icons: {
-    iconfont: "md",
+    iconfont: 'md',
     values: {
-      add: "add",
-      edit: "edit",
-      close: "close",
-      github: "fab fa-github",
-      google: "fab fa-google",
-      facebook: "fab fa-facebook",
-      twitter: "fab fa-twitter"
+      add: 'add',
+      edit: 'edit',
+      close: 'close',
+      facebook: 'fab fa-facebook',
+      twitter: 'fab fa-twitter',
+      youtube: 'fab fa-youtube'
     }
   }
 };
@@ -38,4 +37,4 @@ new Vue({
   router,
   vuetify: new Vuetify(opts),
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
