7d9d649180bca6fe775a4015470571ec14299f6f
Shan Rauf <s.rauf124@gmail.com>
2020-01-17 23:43:46 -0800

    Fix import error

--- backend/src/api/middlewares/ensureExperimentParticipant.ts
+++ backend/src/api/middlewares/ensureExperimentParticipant.ts
@@ -1,40 +1,36 @@
 import { Request, Response, NextFunction } from 'express';
-import Container from 'typedi';
+
+// should you import a service instead that accesses the db?
 import { ExperimentParticipant } from '../../models/intermediary/experimentParticipant';
 import { formatDate } from '../../utils';
 
-const experimentParticipantModel = Container.get<typeof ExperimentParticipant>(
-  'ExperimentParticipant'
-);
 const ensureExperimentParticipant = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  experimentParticipantModel
-    .findOne({
-      where: {
-        participantId: req.user.participantId,
-        experimentId: req.params.experimentId
-      }
-    })
-    .then(experimentParticipantRecord => {
-      if (!experimentParticipantRecord) {
-        return res
-          .json({ error: 'You are not registered for this experiment' })
-          .status(403);
-      } else if (experimentParticipantRecord.dropoutDate !== null) {
-        return res
-          .json({
-            error: `You dropped out of this experiment at ${formatDate(
-              experimentParticipantRecord.dropoutDate
-            )}`
-          })
-          .status(403);
-      } else {
-        return next();
-      }
-    });
+  ExperimentParticipant.findOne({
+    where: {
+      participantId: req.user.participantId,
+      experimentId: req.params.experimentId
+    }
+  }).then(experimentParticipantRecord => {
+    if (!experimentParticipantRecord) {
+      return res
+        .json({ error: 'You are not registered for this experiment' })
+        .status(403);
+    } else if (experimentParticipantRecord.dropoutDate !== null) {
+      return res
+        .json({
+          error: `You dropped out of this experiment at ${formatDate(
+            experimentParticipantRecord.dropoutDate
+          )}`
+        })
+        .status(403);
+    } else {
+      return next();
+    }
+  });
 };
 
 export default ensureExperimentParticipant;
