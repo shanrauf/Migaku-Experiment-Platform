d0986a789b9dd528f28eb824c5ad04817fb7751a
Shan Rauf <s.rauf124@gmail.com>
2020-04-03 03:12:55 -0700

    Add more intervals

--- frontend/src/utils/items.ts
+++ frontend/src/utils/items.ts
@@ -187,11 +187,20 @@ export const fieldsOfOccupation: String[] = [
 export const generalTimeFrames: String[] = [
   '0 months',
   '1 month',
+  '2 months',
   '3 months',
   '6 months',
+  '9 months',
   '1 year',
   '2 years',
+  '3 years',
+  '4 years',
   '5 years',
+  '6 years',
+  '7 years',
+  '8 years',
+  '9 years',
+  '10 years',
   '10+ years'
 ];
 
